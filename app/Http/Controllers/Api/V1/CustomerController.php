248ede841a38302a2772590e3168cc927dd85773
OrdAlex <alejandro@profitintelligence.net>
2023-02-18 21:15:42 +0100

    add filtering

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -8,7 +8,9 @@ use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
+use App\Services\V1\CustomerQuery;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class CustomerController extends Controller
@@ -16,9 +18,16 @@ class CustomerController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): CustomerCollection
+    public function index(Request $request): CustomerCollection
     {
-        return new CustomerCollection(Customer::paginate());
+        $filter = new CustomerQuery();
+        $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
+
+        if (count($queryItems) == 0) {
+            return new CustomerCollection(Customer::paginate());
+        }
+
+        return new CustomerCollection(Customer::where($queryItems)->paginate());
     }
 
     /**
